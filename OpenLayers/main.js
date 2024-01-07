9e46ffda5201ac1e5ea12077763f7f3485d5d3f3
Aixufey <jack_11728@msn.com>
2024-01-07 22:52:20 +0100

    Overlays

--- OpenLayers/main.js
+++ OpenLayers/main.js
@@ -1,4 +1,6 @@
 window.onload = init;
+// Grab the element
+const popupContainerEle = queryDOM("popup-container");
 
 /**
  * @description Initializes a map object
@@ -11,14 +13,51 @@ function init() {
         view: new ol.View({
             center: [0, 0],
             zoom: 2,
-            maxZoom: 10,
-            minZoom: 2,
         }),
+        // Tile is base layer, we can add vector and raster on top
         layers: [
             new ol.layer.Tile({
                 source: new ol.source.OSM(),
             }),
         ],
         target: "js-map",
+    });
+
+    // Create an overlay
+    const popup = createOverlay(popupContainerEle);
+    // Adding overlay to map
+    map.addOverlay(popup);
+    map.on("click", (e) => {
+        const coord = e.coordinate;
+        popup.setPosition(undefined);
+        popup.setPosition(coord);
+        popupContainerEle.innerHTML = coord;
+    });
+}
+
+/**
+ *
+ * @param {*} id of the element
+ * @returns element from DOm
+ */
+function queryDOM(id) {
+    if (!id || id.length === 0) {
+        throw new Error("id is not defined");
+    }
+    return document.querySelector(`#${id}`);
+}
+
+/**
+ * @description https://openlayers.org/en/latest/apidoc/module-ol_Overlay-Overlay.html
+ * @param {*} container element to be added as overlay
+ * @returns overlay object with mirrored positioning
+ */
+function createOverlay(container) {
+    if (!container || container.length === 0) {
+        throw new Error("Container is not defined");
+    }
+    return new ol.Overlay({
+        element: container,
+        positioning: "bottom-left",
     });
 }
